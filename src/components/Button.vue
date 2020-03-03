8c0d41fdfba0ca36c6d3c78db23f504f7371430a
marick <mmaarriicckk@gmail.com>
2020-03-03 17:51:34 +0800

    add

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="Button">
+        {{name}}
+    </button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: {
+    name: String
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ stories/2-Button_test.stories.js
@@ -0,0 +1,28 @@
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import MyButton from '../src/components/Button';
+
+export default {
+    title: 'Button_test',
+    component: MyButton,
+  };
+export const Text = () => ({
+  components: { MyButton },
+  template: '<my-button @click="action" name="test">Hello Button</my-button>',
+  methods: { action: action('clicked') },
+});
+
+export const Jsx = () => ({
+  components: { MyButton },
+  render(h) {
+    return <my-button onClick={this.action} name="test">With JSX</my-button>;
+  },
+  methods: { action: linkTo('clicked') },
+});
+
+export const Emoji = () => ({
+  components: { MyButton },
+  template: '<my-button @click="action" name="test">😀 😎 👍 💯</my-button>',
+  methods: { action: action('clicked') },
+});
